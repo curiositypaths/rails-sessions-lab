592d1df9b5fc3767e561aea93fe2d8063f9c306a
curiositypaths <curiositypaths@users.noreply.github.com>
2017-05-10 13:38:29 -0400

    Sign in user upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save ? redirect_to(private_path) : render(:new)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to(private_path)
+    else
+      render(:new)
+    end
   end
 
   private
