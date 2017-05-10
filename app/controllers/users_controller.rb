ae612c1733c9e2190d8053d55d7d29471e050896
curiositypaths <curiositypaths@users.noreply.github.com>
2017-05-09 21:42:25 -0400

    Add ability to signup users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save ? redirect_to(private_path) : render(:new)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'public#index'
-  get '/private', to: 'private#index'
+  get '/private', to: 'private#index'#, as: 'private_path'
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
 end
